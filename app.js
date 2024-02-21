8fdc7eb21823423dca568917900bd3eab243952a
Karin Ochayon <karinoch070@gmail.com>
2024-02-21 22:29:26 +0200

    Adding info :)

--- app.js
+++ app.js
@@ -51,3 +51,87 @@ changeText();
 setInterval(changeText, 4000);
 
 // ============================================================================================================
+
+// Transfer the data from the form to google sheet
+const scriptURL =
+	"https://script.google.com/macros/s/AKfycbyJ8RQUj7MhHQNC3-EIPVpjHJNq6qEUJRZq3WFq5bwikEinYENvhEH-790n6ONyTmgk/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	msg.innerHTML = "טוען...";
+	fetch(scriptURL, {
+		method: "POST",
+		body: new FormData(form),
+		mode: "cors",
+	})
+		.then((response) => response.text())
+		.then((data) => {
+			console.log("Response from Google Sheets:", data);
+			triggerConfetti();
+			msg.innerHTML = "תודה! ההודעה נשלחה בהצלחה, אחזור אלייך בהקדם";
+			setTimeout(() => {
+				msg.innerHTML = "";
+			}, 5000);
+			form.reset();
+		})
+		.catch((error) => {
+			console.log({ error });
+			msg.innerHTML = "אופסי, משהו השתבש וההודעה אינה נשלחה, אנא נסה שנית";
+			setTimeout(() => {
+				msg.innerHTML = "";
+			}, 5000);
+			form.reset();
+		});
+});
+
+// ============================================================================================================
+
+// Get the current year
+let currentYear = new Date().getFullYear();
+
+// Update the content of the span element with the current year
+document.getElementById("currentYear").textContent = currentYear;
+
+// ============================================================================================================
+
+function triggerConfetti() {
+	var canvas = document.getElementById("confetti-canvas");
+	var ctx = canvas.getContext("2d");
+	var emojis = ["🎉", "🎊", "✨", "💪🏼", "⭐"];
+	var particles = [];
+
+	// Add particles with random positions and lifespans
+	for (var i = 0; i < 100; i++) {
+		particles.push({
+			x: Math.random() * canvas.width,
+			y: Math.random() * canvas.height,
+			size: Math.random() * 20 + 10,
+			emoji: emojis[Math.floor(Math.random() * emojis.length)],
+			velocityY: Math.random() * 0.2 + 0.2, // Adjust velocity for slower movement
+			lifespan: Math.random() * 5000, // Set lifespan to 5 seconds
+		});
+	}
+
+	// Update and draw particles
+	function update() {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+		particles.forEach(function (particle, index) {
+			particle.y += particle.velocityY;
+
+			ctx.font = particle.size + "px Arial";
+			ctx.fillText(particle.emoji, particle.x, particle.y);
+
+			// Remove particles that exceed their lifespan
+			particle.lifespan -= 1000 / 60; // 60 frames per second
+			if (particle.lifespan <= 0) {
+				particles.splice(index, 1);
+			}
+		});
+
+		requestAnimationFrame(update);
+	}
+	update();
+}
